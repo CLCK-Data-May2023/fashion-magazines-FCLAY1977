f7b991cbc5f580b79710153e41d66028caf5cfdc
Fred Clay <fred.clay1977@gamil.com>
2023-10-22 18:24:24 -0400

    Updated code sql

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,14 @@
--- Add your sql here
+SELECT
+    c.customer_name AS Customer,
+    PRINTF('$%.2f', SUM(s.price_per_month * s.subscription_length)) AS "Amount Due"
+FROM
+    customers AS c
+INNER JOIN
+    orders AS o ON c.customer_id = o.customer_id
+INNER JOIN
+    subscriptions AS s ON o.subscription_id = s.subscription_id
+WHERE
+    o.order_status = 'unpaid'
+    AND s.description = 'Fashion Magazine'
+GROUP BY
+    c.customer_name;
